1ab532bb771b55417ba87d370dc4b35ed189bb73
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-02-22 07:05:58 +0000

    Cartas Aleatorias Finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function actualizarPalo() {
+  let palo = ["♥", "♦", "♣", "♠"];
+  let indexPalo = Math.floor(Math.random() * palo.length);
+  return palo[indexPalo];
+}
+
+function actualizarNumero() {
+  let numero = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let indexNumero = Math.floor(Math.random() * numero.length);
+  return numero[indexNumero];
+}
+
+function actualizarCarta() {
+  const randomPalo = actualizarPalo();
+  const carta = document.querySelector(".card");
+
+  document.querySelector(".palo-arriba").innerHTML = randomPalo;
+  document.querySelector(".palo-abajo").innerHTML = randomPalo;
+  document.querySelector(".numero").innerHTML = actualizarNumero();
+
+  switch (randomPalo) {
+    case "♥":
+      carta.className = "card corazon";
+      break;
+    case "♦":
+      carta.className = "card diamante";
+      break;
+    case "♣":
+      carta.className = "card trebol";
+      break;
+    case "♠":
+      carta.className = "card pica";
+      break;
+  }
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  actualizarCarta();
+  document.querySelector("#boton").addEventListener("click", actualizarCarta);
+  setInterval(actualizarCarta, 10000);
 };
